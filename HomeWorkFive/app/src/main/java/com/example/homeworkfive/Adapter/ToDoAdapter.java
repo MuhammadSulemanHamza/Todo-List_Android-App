bca4d6ee6bcc357bc97cbfdee22f9eeaeda119a0
unknown <muhammadsuleman964@gmail.com>
2020-11-09 20:11:31 +0500

    first commit

---
+++ HomeWorkFive/app/src/main/java/com/example/homeworkfive/Adapter/ToDoAdapter.java
@@ -0,0 +1,103 @@
+package com.example.homeworkfive.Adapter;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.homeworkfive.AddNewTask;
+import com.example.homeworkfive.MainActivity;
+import com.example.homeworkfive.Model.ToDoModel;
+import com.example.homeworkfive.R;
+import com.example.homeworkfive.Utils.DatabaseHadnler;
+
+import java.util.List;
+
+public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> {
+
+    private List<ToDoModel> todoList;
+    private MainActivity activity;
+    private DatabaseHadnler db;
+
+    public ToDoAdapter(DatabaseHadnler db, MainActivity activity) {
+        this.db = db;
+        this.activity = activity;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.task_layout, parent, false);
+        return (new ViewHolder(itemView));
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        db.openDataBase();
+        final ToDoModel item = todoList.get(position);
+        holder.task.setText(item.getTask());
+        holder.task.setChecked(toBoolean(item.getStatus()));
+
+        holder.task.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    db.updateStatus(item.getId(),1);
+                }
+                else {
+                    db.updateStatus(item.getId(), 0);
+                }
+            }
+        });
+    }
+
+    private boolean toBoolean(int n){
+        return n!=0;
+    }
+
+    @Override
+    public int getItemCount() {
+        return todoList.size();
+    }
+
+    public void setTasks(List<ToDoModel> todoList){
+        this.todoList = todoList;
+        notifyDataSetChanged();
+    }
+    public Context getContext(){
+        return activity;
+    }
+
+    public void deleteItem(int position){
+        ToDoModel item = todoList.get(position);
+        db.deleteTask(item.getId());
+        todoList.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public void editItem(int position){
+        ToDoModel item = todoList.get(position);
+        Bundle bundle = new Bundle();
+        bundle.putInt("id", item.getId());
+        bundle.putString("task", item.getTask());
+        AddNewTask fragment = new AddNewTask();
+        fragment.setArguments(bundle);
+        fragment.show(activity.getSupportFragmentManager(), AddNewTask.TAG);
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        CheckBox task;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            task = itemView.findViewById(R.id.todoCheckBox);
+        }
+    }
+}
---
+++ HomeWorkFive/app/src/main/java/com/example/homeworkfive/DialogCloseListner.java
@@ -0,0 +1,7 @@
+package com.example.homeworkfive;
+
+import android.content.DialogInterface;
+
+public interface DialogCloseListner {
+    public void handleDialogClose(DialogInterface dialog);
+}
